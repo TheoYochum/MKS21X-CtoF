5c6f6d74770032943d5242ea7e74d1086aa329ca
Theo Yochum <theoyochum@gmail.com>
2021-09-23 20:38:12 -0400

    CtoF renamed CtoFTester, CtoF tested and works with some test cases

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,21 +2,21 @@
 //The functions should both return an int or double depending on how we want it to function I will likely do a double as it will only divide by 3
 import java.util.Scanner;
 
-public class CtoFTester{
+public class CtoFTester {
   public static void main(String[] args) {
     double x;
     Scanner in = new Scanner(System.in);
-    x = in.nextDouble()
+    x = in.nextDouble();
 
     System.out.println(celsiusToFahrenheit(x));
-    System.out.println(fahrenheitToCelsius(x));
+    //System.out.println(fahrenheitToCelsius(x));
   }
 
   public static double celsiusToFahrenheit(double input) {
-
+    return (input * (9.0 / 5.0) + 32);
   }
 
   public static double fahrenheitToCelsius(double input) {
-
+    return 0;
   }
 }
