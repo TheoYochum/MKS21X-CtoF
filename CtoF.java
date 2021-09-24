0f3a2dbdaa6f2228256d45c8b2f671be2ff59e54
Theo Yochum <theoyochum@gmail.com>
2021-09-23 20:32:37 -0400

    Added main fuction with Scanner test functionality

--- CtoF.java
+++ CtoF.java
@@ -1,9 +1,22 @@
+//The functions should each take in a temperature value that could be an int or double based on how we want it to function, I will probably do doubles
+//The functions should both return an int or double depending on how we want it to function I will likely do a double as it will only divide by 3
+import java.util.Scanner;
+
 public class CtoFTester{
-  public static void celsiusToFahrenheit() {
+  public static void main(String[] args) {
+    double x;
+    Scanner in = new Scanner(System.in);
+    x = in.nextDouble()
+
+    System.out.println(celsiusToFahrenheit(x));
+    System.out.println(fahrenheitToCelsius(x));
+  }
+
+  public static double celsiusToFahrenheit(double input) {
 
   }
 
-  public static void fahrenheitToCelsius() {
-    
+  public static double fahrenheitToCelsius(double input) {
+
   }
 }
